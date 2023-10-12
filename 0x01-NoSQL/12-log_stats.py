7d6640e44f0ca03eb61cb4a49a2cfdf3a2241fc4
Henree001 <okaforhenry779@gmail.com>
2023-10-12 22:21:24 +0100

    fixed errors

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,26 +4,26 @@
 from pymongo import MongoClient
 
 
+def print_nginx_logs():
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    db = client.logs
+    nginx_collection = db.nginx
+
+    number_of_documents = nginx_collection.count_documents({})
+    print("{} logs".format(number_of_documents))
+
+    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print("Methods:")
+    for http_method in http_methods:
+        count = nginx_collection.count_documents({"method": http_method})
+        print("\tmethod {}: {}".format(http_method, count))
+
+    status_check_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print("{} status check".format(status_check_count))
+
+
 if __name__ == "__main__":
-
-    def print_nginx_logs():
-        client = MongoClient("mongodb://127.0.0.1:27017")
-        db = client.logs
-        nginx_collection = db.nginx
-
-        number_of_documents = nginx_collection.count_documents({})
-        print("{} logs".format(number_of_documents))
-
-        http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-        print("Methods:")
-        for http_method in http_methods:
-            count = nginx_collection.count_documents({"method": http_method})
-            print("\tmethod {}: {}".format(http_method, count))
-
-        status_check_count = nginx_collection.count_documents(
-            {"method": "GET", "path": "/status"}
-        )
-        print("{} status check".format(status_check_count))
-
     print_nginx_logs()
